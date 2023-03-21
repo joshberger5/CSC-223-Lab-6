7f1b74b53b9a64eb488f570294330a3359b603fb
joshberger5 <joshberger5@yahoo.com>
2023-03-21 15:32:51 -0400

    updated ListMinHeap

--- src/heap/ListMinHeap.java
+++ src/heap/ListMinHeap.java
@@ -37,7 +37,48 @@ public class ListMinHeap<T> implements MinHeap<T>
 	public boolean isEmpty() { return _size == 0; }
 	public int size() { return _size; }
 
-    //
-	// TODO
-	//
+	@Override
+	public void build(List<Double> keys, List<T> values) {
+		for (int i = 0; i < keys.size(); i ++) {
+			HeapNode<T> node = new HeapNode(keys.get(i), values.get(i));
+			_heap[_size++] = node;
+		}
+	}
+
+	@Override
+	public void insert(double key, T value) {
+		HeapNode<T> node = new HeapNode(key, value);
+		_heap[_size++] = node;
+	}
+
+	@Override
+	public HeapNode<T> extractMin() {
+		HeapNode<T> min = _heap[0];
+		int minIndex = 0;
+		// finds the HeapNode with the smallest key and saves its index
+		for (int i = 1; i < _size; i++) {
+			int cmp = min.compareTo(_heap[i]);
+			if (cmp > 0) {
+				min = _heap[i];
+				minIndex = i;
+			}
+		}
+		// shifts every HeapNode after the minimum's index down one
+		for (int i = minIndex; i < _size-1; minIndex++) {
+			_heap[i] = _heap[i+1];
+		}
+		_heap[_size-1] = null;
+		_size--;
+		return min;
+	}
+
+	@Override
+	public HeapNode<T> peekMin() {
+		HeapNode<T> min = _heap[0];
+		for (int i = 1; i < _size; i++) {
+			int cmp = min.compareTo(_heap[i]);
+			if (cmp > 0) min = _heap[i];
+		}
+		return min;
+	}
 }
